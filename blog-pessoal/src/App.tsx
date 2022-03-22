2ca07ee6c3aa032931a12633830ca6aaa4cd5265
carol-alonso <lacerdacarol01@gmail.com>
2022-03-22 19:52:06 -0300

    Teste 1

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home'
+
+let nome = 'carol';
+function App() {
+  return (
+  <Home />
+  );
+}
+
+export default App;
---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img className="img" src="https://www.marketing365.com.br/wp-content/uploads/2018/05/segredos-bom-trabalho-equipe-1030x687.png"/>
+        </>
+    );
+}
+
+export default Home;
